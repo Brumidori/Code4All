442bd1590d649f8fc3ba241d2adcc03b6aa3cdf2
luizavramos <luizavramos30@gmail.com>
2022-08-06 12:29:31 -0300

    Criando controllers Like post, like comentario, comentario e post

--- src/main/java/com/socialnetwork/controller/LikeComentarioController.java
+++ src/main/java/com/socialnetwork/controller/LikeComentarioController.java
@@ -1,13 +1,24 @@
 package com.socialnetwork.controller;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.socialnetwork.model.LikeComentario;
 import com.socialnetwork.repository.LikeComentarioRepository;
@@ -18,10 +29,36 @@ import com.socialnetwork.repository.LikeComentarioRepository;
 public class LikeComentarioController {
 
 	@Autowired
-	private LikeComentarioRepository LikeComentarioRepository;
+	private LikeComentarioRepository likeComentarioRepository;
 	
 	@GetMapping
 	public ResponseEntity<List<LikeComentario>> getAll(){
-		return ResponseEntity.ok(LikeComentarioRepository.findAll());
+		return ResponseEntity.ok(likeComentarioRepository.findAll());
+	}
+	@GetMapping("/{id}")
+	public ResponseEntity<LikeComentario> getById(@PathVariable long id){
+		return likeComentarioRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta)).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		
+	}	
+		
+	@PostMapping
+	public ResponseEntity<LikeComentario> post(@Valid @RequestBody LikeComentario likeComentario){
+		return ResponseEntity.status(HttpStatus.CREATED).body(likeComentarioRepository.save(likeComentario));
+	}
+	
+	@PutMapping
+	public ResponseEntity<LikeComentario> put(@Valid @RequestBody LikeComentario likeComentario){
+		return likeComentarioRepository.findById(likeComentario.getId()).map(resposta -> ResponseEntity.status(HttpStatus.OK).body(likeComentarioRepository.save(likeComentario))).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		Optional<LikeComentario> likeComentario = likeComentarioRepository.findById(id);
+		
+		if(likeComentario.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		likeComentarioRepository.deleteById(id);
 	}
 }
--- src/main/java/com/socialnetwork/controller/LikePostController.java
+++ src/main/java/com/socialnetwork/controller/LikePostController.java
@@ -1,13 +1,24 @@
 package com.socialnetwork.controller;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.socialnetwork.model.LikePost;
 import com.socialnetwork.repository.LikePostRepository;
@@ -23,5 +34,32 @@ public class LikePostController {
 	@GetMapping
 	public ResponseEntity<List<LikePost>> getAll(){
 		return ResponseEntity.ok(likePostRepository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<LikePost> getById(@PathVariable long id){
+		return likePostRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta)).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		
+	}	
+		
+	@PostMapping
+	public ResponseEntity<LikePost> post(@Valid @RequestBody LikePost likePost){
+		return ResponseEntity.status(HttpStatus.CREATED).body(likePostRepository.save(likePost));
+	}
+	
+	@PutMapping
+	public ResponseEntity<LikePost> put(@Valid @RequestBody LikePost likePost){
+		return likePostRepository.findById(likePost.getId()).map(resposta -> ResponseEntity.status(HttpStatus.OK).body(likePostRepository.save(likePost))).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		Optional<LikePost> likePost = likePostRepository.findById(id);
+		
+		if(likePost.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		likePostRepository.deleteById(id);
 	}
 }
